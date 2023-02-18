505c87974c1e6f427df1d82cd4a8beebfd71931d
Suphanat <suphanat.swr@gmail.com>
2023-02-18 17:09:22 +0700

    modified: main.py - added a looping  for user to search multiple company in one session - keep data from sec-data-pull as variable sec_data

--- main.py
+++ main.py
@@ -3,13 +3,26 @@ import secDataPull as sec
 from ticker_check import utility
 
 if __name__ == "__main__":
-    company = input("Please enter a company you would like to learn about:")
-    try:
-        util = utility()
-        ticker = util.get_ticker(company)
-        cik = tm.tickerToCIK(ticker)
-        print(sec.getSecData(cik))
 
-    except Exception as e: 
-        print("Sorry, looks like we don't have any details on the company you provided at this time.")
-        print(e)
+    # Main loop continue until user enter 'quit'.
+
+    stop = False
+    while not stop:
+        company = input("Please enter a company you would like to learn about(enter 'quit' to quit):")
+
+        if company != 'quit' :
+            util = utility()
+            ticker = util.get_ticker(company)
+
+            try:
+                cik = tm.tickerToCIK(ticker)
+                sec_data = sec.getSecData(cik)
+                print(sec_data)
+
+            except Exception as e: 
+                print("Sorry, looks like we don't have any details on the company you provided at this time.")
+                print(e)
+        else:
+            stop = True
+    
+    print("Thank you for using our sevices.")
