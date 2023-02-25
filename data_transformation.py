0de6c63014b2bfa7871a43d9a4767cd23c80fd5e
AshnaDabas <ashnadabas@gmail.com>
2023-02-24 22:15:46 -0500

    clean up

--- data_transformation.py
+++
@@ -1,4 +0,0 @@
-def setup_company_data(company_list, input_source_std1, input_source_std2, input_source_std3):
-    print("Setting up company data")
-    company_dict = dict(zip(iter(company_list), ''))
-    return company_dict
