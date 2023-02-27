f048228c221bdba8eea5727ded50012d43277e95
Pat Swr <109974034+swrpat@users.noreply.github.com>
2023-02-27 22:23:40 +0700

    Add files via upload
    upload from Nitin's email

--- yahooFinanceWebsite.py
+++ yahooFinanceWebsite.py
@@ -6,6 +6,98 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from urllib.request import urlopen, Request
 
+def getCompanyStatistics(stock_ticker):
+    '''
+    Function Description:
+        Python program that pulls company's executives list from Yahoo Finance website.
+    Arguments:
+        Stock Ticker as a string
+    Return Value:
+        A DataFrame with a list of company executives along with their titles.
+        None if an empty string is passed, or if the website retrurns no data. 
+    '''
+    
+    # Pre-Condition
+    if len(stock_ticker)==0: return
+    
+    # Yahoo Finace Page Link
+    url_website = 'https://finance.yahoo.com/quote/{}/key-statistics?p={}'.format(stock_ticker,stock_ticker)
+    #print(url_website)
+    
+    try:
+        # make a http request call to the yahoo finace website 
+        req = Request(url_website, headers={'User-Agent': 'Mozilla/5.0'})
+        html = urlopen(req)
+        # load response data to Beautiful Soup
+        bs_website = BeautifulSoup(html.read(), "lxml")
+        
+        #fname = 'bs_website_company_statistics.txt'
+        #fout = open(fname, 'wt', encoding='utf-8')
+        #fout.write(str(bs_website))
+        #fout.close()
+        #with open(fname, 'r') as f:
+        #    page = f.read()
+        #    f.close()
+
+        # Get a list of HTML tables on the web page
+        table_list = bs_website.findAll('table')
+
+        # quick check to see if expected table is returned
+        if len(table_list) == 0: return
+        
+        # The first tale has the financial statistics we seek
+        table_company_statistics = table_list[0]
+        
+        # declare & instantiate variables
+        dict_stats = {}
+        valuation_measures = []
+        raw_data = []
+        header = []
+        
+        # loop through the table to extract statistics header and data
+        for i, row in enumerate(table_company_statistics.findAll('tr')):
+
+            # The row has the header info  
+            if i == 0:
+        
+                #loop through the header information
+                for j, col in enumerate(row.findAll('th')):
+                    
+                    # Simplify the header text for this specific column. The rest col header text are Ok.
+                    if j == 1:
+                        header = ['Current']
+                    else:
+                        header.append(col.text)
+
+            else:
+                
+                fin_data = []
+                
+                #loop through the statistics label and data
+                for j, col in enumerate(row.findAll('td')):
+                    
+                    # the first column is the label
+                    if j == 0:
+                        valuation_measures.append(col.text.strip())
+                        label = col.text
+                    else:
+                        fin_data.append(col.text)
+                
+                # add to a dict
+                dict_stats[label] = fin_data
+                raw_data.append(fin_data)
+
+        # convert to a dataframe                
+        df_statistics = pd.DataFrame(raw_data, columns=header, index = valuation_measures)
+
+        # return dataframe
+        return df_statistics
+        
+    except:
+        print("Exception: {}".format(sys.exc_info()))
+        #pass
+
+
 
 def getListOfCompanyExecutives(stock_ticker):
     '''
@@ -74,6 +166,18 @@ def getListOfCompanyExecutives(stock_ticker):
 if __name__ == '__main__':
 
     # Unit Test 1: Valid Stock Ticker
+    df = getCompanyStatistics('AMZN')
+    print(df)
+    
+    # Unit Test 2: No Stock Ticker
+    df = getCompanyStatistics('')
+    print(df)
+    
+    # Unit Test 3: Invalid Stock Ticker
+    df = getCompanyStatistics('AMZN231')
+    print(df)
+    
+    # Unit Test 1: Valid Stock Ticker
     df = getListOfCompanyExecutives('AMZN')
     print(df)
     
@@ -84,3 +188,4 @@ if __name__ == '__main__':
     # Unit Test 3: Invalid Stock Ticker
     df = getListOfCompanyExecutives('AMZN231')
     print(df)
+    
